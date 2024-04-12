1f9f7e6f2f35f7d0a57a5512b0683830fd7cc638
Rafael Abbade <rafaelvabbade@gmail.com>
2024-04-12 10:08:02 -0300

    Initial commit from template

--- verification/referee.py
+++ verification/referee.py
@@ -38,7 +38,7 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "{{FUNC}}",
+            "python": "calcular_custo_hotel",
             "js": "countConsecutiveSummers"
         },
         cover_code={
--- verification/tests.py
+++ verification/tests.py
@@ -10,6 +10,29 @@ Each test is a dict with
 
 TESTS = {
     "Basics": [
-        {{TESTES}}
+        {
+        "input": [100, 10, 1],
+        "answer": 3100
+    },
+    {
+        "input": [100, 20, 15],
+        "answer": 6460
+    },
+    {
+        "input": [100, 5, 16],
+        "answer": 2720
+    },
+    {
+        "input": [150, 10, 15],
+        "answer": 8050
+    },
+    {
+        "input": [200, 25, 31],
+        "answer": 200
+    },
+    {
+        "input": [50, 5, 2],
+        "answer": 4575
+    }
     ]
 }
